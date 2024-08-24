d0e140307ef4f545410199ec0a76faaf5a6c1c7b
pankaj mandal <singhswami504@gmail.com>
2024-08-24 11:31:08 +0530

    first commit

---
+++ weather.js
@@ -0,0 +1,50 @@
+const apiKey = "5b2df9b20fc3e9f054ca2256c7405e93";
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?&units=metric&q=";
+const searchBox = document.querySelector(".search input");
+const searchBtn = document.querySelector(".search button");
+const weatherIcon = document.querySelector(".Weather-Icon");
+
+
+async function checkWeather(city){
+    const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
+    
+    if(response.status == 404){
+        document.querySelector(".error").style.display = "block";
+        document.querySelector(".weather").style.display = "none";
+    }else{
+        var data = await response.json();
+
+
+        document.querySelector(".city").innerHTML = data.name;
+        document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°C";
+        document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
+        document.querySelector(".wind").innerHTML = data.wind.speed + " km/hr "; 
+        
+    
+        if(data.weather[0].main == "Clouds"){
+            weatherIcon.src = "weather-app-img/images/clouds.png";
+        }
+        else if(data.weather[0].main == "Clear"){
+            weatherIcon.src = "weather-app-img/images/clear.png";
+        }
+        else if(data.weather[0].main == "Rain"){
+            weatherIcon.src = "weather-app-img/images/rain.png";
+        }
+        else if(data.weather[0].main == "Drizzle"){
+            weatherIcon.src = "weather-app-img/images/drizzle.png";
+        }
+        else if(data.weather[0].main == "Mist"){
+            weatherIcon.src = "weather-app-img/images/mist.png";
+        }
+        document.querySelector(".weather").style.display = "block";
+        document.querySelector(".error").style.display = "none";
+    }
+    }
+    
+   
+
+searchBtn.addEventListener("click", ()=> {
+    checkWeather(searchBox.value);
+})
+
+
